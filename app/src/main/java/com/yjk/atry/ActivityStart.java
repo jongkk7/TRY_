37789c7a98d0136035eab6ab6a22484399da69ca
YJK <jongkwon.you@lookinbody.com>
2021-11-19 22:38:39 +0900

    21.11.19 gitignore 수정 Hashkey 유틸추가

--- app/src/main/java/com/yjk/atry/ActivityStart.java
+++ app/src/main/java/com/yjk/atry/ActivityStart.java
@@ -2,9 +2,11 @@ package com.yjk.atry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.yjk.common.HashKeyUtil;
 import com.yjk.yjk.ActivityIntro;
 
 
@@ -15,16 +17,29 @@ public class ActivityStart extends AppCompatActivity {
      * start activity
      */
     private Class activityClass = ActivityIntro.class;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        init();
         start();
     }
 
     private void start(){
         startActivity(new Intent(this, activityClass));
+    }
+
+    private void init(){
+
+        context = this;
+
+        if(BuildConfig.DEBUG){
+
+            HashKeyUtil.getHashKey(context);
+
+        }
     }
 }
