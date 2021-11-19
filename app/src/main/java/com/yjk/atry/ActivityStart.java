a2f3a5e5be5be713fdc0d7351bfc355cfdcfc59f
nhsmart95 <nhsmart95@nhsmart95ui-MacBookPro.local>
2021-11-18 17:28:55 -0800

    first commit

---
+++ app/src/main/java/com/yjk/atry/ActivityStart.java
@@ -0,0 +1,15 @@
+package com.yjk.atry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class ActivityStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
