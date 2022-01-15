ba0394033fb2e6fe022f1527ba1d5b2be1bbd68f
YJK <jongkwon.you@lookinbody.com>
2022-01-15 21:09:02 +0900

    room, http 추가중

---
+++ app/src/main/java/com/yjk/atry/A/A_.java
@@ -0,0 +1,8 @@
+package com.yjk.atry.A;
+
+public class A_ {
+
+    private int a = 1;
+    private int b = 1;
+    private int c = 1;
+}
---
+++ app/src/main/java/com/yjk/atry/A/B_.java
@@ -0,0 +1,6 @@
+package com.yjk.atry.A;
+
+public class B_ {
+
+    int a = 0;
+}
